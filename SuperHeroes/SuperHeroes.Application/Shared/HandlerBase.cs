ac996a528df538b8fb6b49d3197fc233c0527686
Alexander Malyshkin <alexander.malyshkin@gmail.com>
2024-02-19 00:01:35 +0100

    comments for HandlerBase

--- SuperHeroes/SuperHeroes.Application/Shared/HandlerBase.cs
+++ SuperHeroes/SuperHeroes.Application/Shared/HandlerBase.cs
@@ -21,6 +21,12 @@ public abstract class HandlerBase<TRequest, TResponse> : IRequestHandler<TReques
         _validator = validator;
     }
     
+    /// <summary>
+    /// Validates the request before processing it (utilizing the template method pattern)
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="ct"></param>
+    /// <returns></returns>
     public async Task<TResponse> Handle(TRequest request, CancellationToken ct)
     {
         ValidationResult? validationResult = await _validator.ValidateAsync(request, ct);
@@ -32,9 +38,18 @@ public abstract class HandlerBase<TRequest, TResponse> : IRequestHandler<TReques
     
     private TResponse ProduceValidationErrorResponse(ValidationResult validationResult)
     {
+        // we construct a response with the validation errors:
         var details = string.Join(". ", validationResult.Errors.Select(x => x.ErrorMessage));
         return ConstructSpecificValidationErrorResponse(ValidationFailed, details, validationResult.IsValid);
     }
+    
+    /// <summary>
+    /// Constructs a specific validation error response (in derived classes)
+    /// </summary>
+    /// <param name="errorTitle"></param>
+    /// <param name="details"></param>
+    /// <param name="isValid"></param>
+    /// <returns></returns>
     protected abstract TResponse ConstructSpecificValidationErrorResponse(string errorTitle, string details, bool isValid);
     protected abstract Task<TResponse> HandleInternal(TRequest request, CancellationToken ct);
 }
